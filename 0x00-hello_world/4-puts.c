96388944f1c7b90ceaa50d8bb7efefbcb5118ae6
Oluwaseun <oluwaseunadesola25@gmail.com>
2022-09-12 21:45:38 -0700

    Print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 #include<stdio.h>
-/**
- * main -program prints with function puts
- *
- * Return: Always 0
- */
+ /**
+  * main -program prints with function puts
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzel");
+return (0);
 }
